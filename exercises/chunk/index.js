84a43b8685ca7b7e7f05c8f4b8db8483fbc826a8
iDatLe <LeDatDac@gmail.com>
2019-02-28 15:27:54 -0800

    Finished chunk exercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const arr = [];
+
+    while (array.length > size) {
+        arr.push(array.splice(0, size));
+    }
+    
+    arr.push(array);
+
+    return arr;
+
+}
+
+console.log(chunk([1,2,3,4,5], 2));
 
 module.exports = chunk;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const hash = {};
+    let max = 0;
+    let result = '';
+
+    for (let char of str) {
+        if (!hash[char]) {
+            hash[char] = 1;
+        } else {
+            hash[char]++;
+        }
+    }
+
+    for (let char in hash) {
+        if (hash[char] > max) {
+            max = hash[char];
+            result = char;
+        }
+    }
+
+    return result;
+
+}
+
+console.log(maxChar('hello'))
 
 module.exports = maxChar;
